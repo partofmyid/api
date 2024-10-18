6a4eac8d4271519fea5e67203075d5321a62e256
Satria <90962949+SX-9@users.noreply.github.com>
2024-10-18 16:13:15 +0700

    git routes and init

---
+++ README.md
@@ -0,0 +1,4 @@
+# part-of.my.id API
+read only api for querying domain information
+
+## routes
---
+++ index.js
@@ -0,0 +1,73 @@
+// @ts-check
+
+import { spawn } from "child_process";
+import express from "express";
+import path from "path";
+import fs from "fs";
+
+const PORT = process.env.PORT || 3000;
+const PASS = process.env.PASS || "password"; // only applies for git actions, to prevent spam cloning or pulling
+const REPO = process.env.REPO_URL || "https://github.com/partofmyid/register";
+const DIR = path.join(process.cwd(), process.env.DIR || "repo");
+
+console.log(`\
+Start settings (process.env to modify):
+PORT: ${PORT}
+PASS: ${PASS}
+REPO: ${REPO}
+DIR: ${DIR}
+`);
+
+const app = express();
+
+app.post("/git/clone", (req, res) => {
+    if (req.query.pass !== PASS) {
+        res.status(401).send("Unauthorized");
+        return;
+    }
+
+    const git = spawn("/usr/bin/env", ["git", "clone", "--depth", "1", "--branch", "main", REPO, DIR]);
+    let stderr = "", stdout = "";
+    git.stdout.on("data", (data) => stdout += data);
+    git.stderr.on("data", (data) => stderr += data);
+
+    git.on("exit", (code) => {
+        if (code === 0) res.send(stdout);
+        else res.status(500).send(stderr);
+    });
+});
+
+app.post("/git/pull", (req, res) => {
+    if (req.query.pass !== PASS) {
+        res.status(401).send("Unauthorized");
+        return;
+    }
+
+    const git = spawn("/usr/bin/env", ["git", "pull"], { cwd: DIR });
+    let stderr = "", stdout = "";
+    git.stdout.on("data", (data) => stdout += data);
+    git.stderr.on("data", (data) => stderr += data);
+
+    git.on("exit", (code) => {
+        if (code === 0) res.send(stdout);
+        else res.status(500).send(stderr);
+    });
+});
+
+app.get("/git/rm" , (req, res) => {
+    if (req.query.pass !== PASS) {
+        res.status(401).send("Unauthorized");
+        return;
+    }
+
+    fs.rmdir(DIR, { recursive: true }, (err) => {
+        if (err) res.status
+        else res.send("Deleted");
+    });
+});
+
+
+app.listen(PORT, () => {
+    if (PASS === "password") console.log("Please change the default password");
+    console.log(`Server is running on port ${PORT}`)
+});
