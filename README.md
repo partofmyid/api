b16bbda51fe46b3014936078a9d43d4696cb1daf
Satria <90962949+SX-9@users.noreply.github.com>
2024-10-25 15:54:21 +0700

    read routes and docs

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 read only api for querying domain information
 
 ## routes
+
+- **GET** `/query/count` - returns registered subdomain count
+- **GET** `/query/check/:subdomain` - checks if :subdomain is available
+- **GET** `/query/subdomain/:subdomains` - gets records for :subdomains (fetch multiple by coma separating subdomains)
+- **GET** `/query/username/:username` - gets subdomains owned by github user :username
+
+> [!NOTE]
+> `/git` and `/db` routes are password protected
+
+- **POST** `/git/clone` - clones registration repository
+- **POST** `/git/pull` - pulls changes to already cloned repo
+- **POST** `/git/rm` - deletes local repository clone directory
+- **POST** `/db/sync` - syncs database with registration repository
+- **GET** `/db/raw` - gets raw db.json
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import path from "path";
 import fs from "fs";
 
 const PORT = process.env.PORT || 3000;
-const PASS = process.env.PASS || "password"; // only applies for git actions, to prevent spam cloning or pulling
+const PASS = process.env.PASS || "password"; // only applies for git and db actions, to prevent spam cloning or pulling
 const REPO = process.env.REPO_URL || "https://github.com/partofmyid/register";
 const DIR = process.env.DIR || "repo";
 
@@ -17,15 +17,21 @@ PASS: ${PASS}
 REPO: ${REPO}
 DIR: ${DIR}
 `);
-
 const app = express();
 
-app.post("/git/clone", (req, res) => {
-    if (req.query.pass !== PASS) {
+app.set("trust proxy", true);
+app.use(express.json());
+app.use((req, res, next) => {
+    if ((req.originalUrl.startsWith("/git") || req.originalUrl.startsWith("/db")) && req.query.pass !== PASS) {
         res.status(401).send("Unauthorized");
         return;
     }
 
+    console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.originalUrl}`);
+    next();
+});
+
+app.post("/git/clone", (_, res) => {
     const git = spawn("git", ["clone", "--depth", "1", "--branch", "main", REPO, DIR]);
     let stderr = "", stdout = "";
     git.stdout.on("data", (data) => stdout += data);
@@ -37,12 +43,7 @@ app.post("/git/clone", (req, res) => {
     });
 });
 
-app.post("/git/pull", (req, res) => {
-    if (req.query.pass !== PASS) {
-        res.status(401).send("Unauthorized");
-        return;
-    }
-
+app.post("/git/pull", (_, res) => {
     if (!fs.existsSync(DIR)) {
         res.status(500).send("Directory not found");
         return;
@@ -59,24 +60,14 @@ app.post("/git/pull", (req, res) => {
     });
 });
 
-app.post("/git/rm" , (req, res) => {
-    if (req.query.pass !== PASS) {
-        res.status(401).send("Unauthorized");
-        return;
-    }
-
+app.post("/git/rm" , (_, res) => {
     fs.rmdir(DIR, { recursive: true }, (err) => {
         if (err) res.status
         else res.send("Deleted");
     });
 });
 
-app.post("/db/sync", (req, res) => {
-    if (req.query.pass !== PASS) {
-        res.status(401).send("Unauthorized");
-        return;
-    }
-
+app.post("/db/sync", (_, res) => {
     const domainsDir = path.join(DIR, "domains");
     const dbPath = path.join(process.cwd(), "db.json");
     const db = []
@@ -95,7 +86,56 @@ app.post("/db/sync", (req, res) => {
     }
 });
 
+app.get("/db/raw", (_, res) => {
+    readDB()
+        .then((db) => res.send(db))
+        .catch((err) => res.status(500).send(err.message));
+});
+
+app.get("/query/count", (_, res) => {
+    readDB()
+        .then((db) => res.send({ count: db.length }))
+        .catch((err) => res.status(500).send(err.message));
+});
+
+app.get("/query/check/:subdomain", (req, res) => {
+    readDB().then((db) => {
+        const data = db.find((entry) => entry.subdomain === req.params.subdomain);
+        let available;
+        if (data) available = false;
+        else available = true;
+        res.send({ available });
+    }).catch((err) => res.status(500).send(err.message));
+});
+
+app.get("/query/subdomain/:subdomains", (req, res) => {
+    readDB().then((db) => {
+        const subdomains = req.params.subdomains.split(",");
+        const result = subdomains.map((subdomain) => {
+            const data = db.find((entry) => entry.subdomain === subdomain);
+            return { subdomain, available: !data };
+        });
+        res.send(result);
+    }).catch((err) => res.status(500).send(err.message));
+});
+
+app.get("/query/username/:username", (req, res) => {
+    readDB().then((db) => {
+        res.send(db.filter((entry) => entry.properties.owner.username === req.params.username));
+    }).catch((err) => res.status(500).send(err.message));
+});
+
 app.listen(PORT, () => {
     if (PASS === "password") console.log("Please change the default password");
     console.log(`Server is running on port ${PORT}`)
 });
+
+function readDB() {
+    return new Promise((resolve, reject) => {
+        const dbPath = path.join(process.cwd(), "db.json");
+        fs.readFile(dbPath, "utf8", (err, data) => {
+            if (err) reject(err);
+            else resolve(JSON.parse(data));
+        });
+    });
+}
